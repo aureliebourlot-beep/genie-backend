b4802315d73bfdacb11a5a06cc595d2cfa302888
aureliebourlot-beep <aurelie.bourlot@gmail.com>
2026-03-14 13:20:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
 
 app = Flask(__name__)
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "HELLO OK"
+
+@app.route("/test", methods=["POST"])
+def test():
+    data = request.get_json()
+
+    question = data.get("message")
+
+    return jsonify({
+        "answer": f"Tu as envoyé: {question}"
+    })
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
